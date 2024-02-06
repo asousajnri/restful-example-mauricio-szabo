fe9e53ee5a270f23c23e9ad5cf5916ebe9c496c2
Antônio <a.sousa.jnri@gmail.com>
2024-02-05 22:19:55 -0300

    Add users handler

--- src/restful_example_mauricio_szabo/core.clj
+++ src/restful_example_mauricio_szabo/core.clj
@@ -6,9 +6,24 @@
 (defn respond-hello [request]
   {:status 200 :body "Hello, Person!"})
 
+(def users [{:name "Maurício" :age 10}
+            {:name "Ana" :age 12}
+            {:name "André" :age 10}])
+
+(defn filter-users [params users]
+  (filter (fn [user] (= params (select-keys user (keys params))))
+          users))
+
+(defn get-users-handler [request]
+  (-> request
+      (:params {})
+      (filter-users users)
+      http/json-response))
+
 (def routes
   (route/expand-routes
-    #{["/greet" :get respond-hello :route-name :greet]}))
+    #{["/greet" :get respond-hello :route-name :greet]
+      ["/users" :get get-users-handler :route-name :users]}))
 
 (def pedestal-config
     {::http/routes (fn [] routes)
